19e1115eb9752139cb10d9baa5bdd0f695261deb
nuuuwan <nuuuwan@gmail.com>
2025-07-05 08:03:37 +0530

    lint

--- src/lld/www/extra_gazettes/__init__.py
+++ src/lld/www/extra_gazettes/__init__.py
@@ -2,7 +2,9 @@
 # flake8: noqa: F408
 
 from lld.www.extra_gazettes.ExtraGazetteMetadata import ExtraGazetteMetadata
-from lld.www.extra_gazettes.ExtraGazettesByYearPage import \
-    ExtraGazettesByYearPage
-from lld.www.extra_gazettes.ExtraGazettesForYearPage import \
-    ExtraGazettesForYearPage
+from lld.www.extra_gazettes.ExtraGazettesByYearPage import (
+    ExtraGazettesByYearPage,
+)
+from lld.www.extra_gazettes.ExtraGazettesForYearPage import (
+    ExtraGazettesForYearPage,
+)
