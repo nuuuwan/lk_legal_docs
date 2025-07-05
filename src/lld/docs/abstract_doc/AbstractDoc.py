1a47ea280304f42fc3e895fc5b55d17aa89bf620
nuuuwan <nuuuwan@gmail.com>
2025-07-05 09:16:45 +0530

    Added AbstractDocDownloader

--- src/lld/docs/abstract_doc/AbstractDoc.py
+++ src/lld/docs/abstract_doc/AbstractDoc.py
@@ -1,13 +1,19 @@
 from utils import Log
 
 from lld.docs.abstract_doc.AbstractDocBase import AbstractDocBase
+from lld.docs.abstract_doc.AbstractDocDownloader import AbstractDocDownloader
 from lld.docs.abstract_doc.AbstractDocReadMe import AbstractDocReadMe
 from lld.docs.abstract_doc.AbstractDocSerializer import AbstractDocSerializer
 
 log = Log("AbstractDoc")
 
 
-class AbstractDoc(AbstractDocBase, AbstractDocSerializer, AbstractDocReadMe):
+class AbstractDoc(
+    AbstractDocBase,
+    AbstractDocSerializer,
+    AbstractDocReadMe,
+    AbstractDocDownloader,
+):
 
     @classmethod
     def get_doc_type_name(cls):
