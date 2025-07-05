8d6212321e60f72eaf560fe92da7cb22175c5f2f
nuuuwan <nuuuwan@gmail.com>
2025-07-05 15:41:18 +0530

    various

---
+++ src/lld/www/common/AbstractPipelineRunner.py
@@ -0,0 +1,39 @@
+from utils import Log
+
+log = Log("AbstractPipelineRunner")
+
+
+class AbstractPipelineRunner:
+
+    @classmethod
+    def get_pipeline_name(cls):
+        raise NotImplementedError
+
+    def gen_docs(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def __process_doc__(doc):
+        try:
+            is_hot = doc.download_all()
+            doc.write()
+            doc.write_readme()
+            doc.extract_text()
+            return is_hot
+        except Exception as e:
+            log.error(f"❌ Error downloading {doc.doc_num}: {e}")
+            return False
+
+    def run_pipeline(self, max_n_hot):
+        log.info(f"🤖 Running {self.get_pipeline_name()}.")
+        n_hot = 0
+        for doc in self.gen_docs():
+            if n_hot >= max_n_hot:
+                log.info(f"🛑 Downloaded {n_hot} new docs.")
+                return
+
+            is_hot = self.__process_doc__(doc)
+            if is_hot:
+                n_hot += 1
+                log.info(f"✅ ({n_hot}/{max_n_hot}) Downloaded {doc}")
+        log.info(f"🛑🛑 Downloaded ALL {self.get_pipeline_name()}.")
--- src/lld/www/common/ByYearPage.py
+++ src/lld/www/common/ByYearPage.py
@@ -1,12 +1,13 @@
 from utils import Log
 
+from lld.www.common.AbstractPipelineRunner import AbstractPipelineRunner
 from lld.www.common.ForYearPage import ForYearPage
 from lld.www.common.WebPage import WebPage
 
 log = Log("ByYearPage")
 
 
-class ByYearPage(WebPage):
+class ByYearPage(WebPage, AbstractPipelineRunner):
 
     @staticmethod
     def __get_url__(doc_cls):
@@ -32,35 +33,3 @@ class ByYearPage(WebPage):
         for for_year_page in self.gen_for_year_pages():
             for doc in for_year_page.gen_docs():
                 yield doc
-
-    @staticmethod
-    def __process_doc__(doc):
-
-        try:
-            is_hot = doc.download_all()
-            doc.write()
-            doc.write_readme()
-            doc.extract_text()
-            return is_hot
-        except Exception as e:
-            log.error(f"❌ Error downloading {doc.doc_num}: {e}")
-            return False
-
-    def run_pipeline(self, max_n_hot):
-        log.info(
-            f"🤖 Running pipeline for {
-                self.doc_cls.get_doc_type_name().title()}."
-        )
-        n_hot = 0
-        for doc in self.gen_docs():
-            if n_hot >= max_n_hot:
-                log.info(f"🛑 Downloaded {n_hot} new docs.")
-                return
-
-            is_hot = self.__process_doc__(doc)
-            if is_hot:
-                n_hot += 1
-                log.info(f"✅ ({n_hot}/{max_n_hot}) Downloaded {doc}")
-        log.info(
-            f"🛑🛑 Downloaded ALL {self.doc_cls.get_doc_type_name().title()}."
-        )
