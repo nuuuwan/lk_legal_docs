0d0d0a362f8963cd48e4e261fcba32ab0b51bbf8
nuuuwan <nuuuwan@gmail.com>
2025-07-05 08:51:54 +0530

    Removed unused

--- src/lld/www/bills/__init__.py
+++
@@ -1,6 +0,0 @@
-# lld.www.bills (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from lld.www.bills.BillMetadata import BillMetadata
-from lld.www.bills.BillsByYearPage import BillsByYearPage
-from lld.www.bills.BillsForYearPage import BillsForYearPage
--- src/lld/www/extra_gazettes/__init__.py
+++
@@ -1,8 +0,0 @@
-# lld.www.extra_gazettes (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from lld.www.extra_gazettes.ExtraGazetteMetadata import ExtraGazetteMetadata
-from lld.www.extra_gazettes.ExtraGazettesByYearPage import \
-    ExtraGazettesByYearPage
-from lld.www.extra_gazettes.ExtraGazettesForYearPage import \
-    ExtraGazettesForYearPage
