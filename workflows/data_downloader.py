385c8b0eb17a9fa4a6d377a9cf0822de78d64a91
nuuuwan <nuuuwan@gmail.com>
2025-08-02 19:13:56 +0530

    default max delta t

--- workflows/data_downloader.py
+++ workflows/data_downloader.py
@@ -8,7 +8,6 @@ from lld import AbstractDoc, DocFactory
 
 log = Log("data_downloader")
 N_BATCH = 8
-DEFAULT_MAX_DELTA_T = 1_200
 
 
 def get_worker(doc):
@@ -29,7 +28,6 @@ def get_doc_list(decade):
 
 
 def main(max_delta_t, decade):
-    max_delta_t = max_delta_t or DEFAULT_MAX_DELTA_T
     log.debug(f"{max_delta_t=:,.1f}s")
     log.debug(f"{decade=}")
     log.debug(f"{N_BATCH=}")
@@ -69,8 +67,8 @@ def get_options():
     )
     parser.add_argument(
         "--max_delta_t",
-        type=str,
-        default="",
+        type=int,
+        default=1_200,
         help="Maximum time to run the downloader in seconds.",
     )
     parser.add_argument(
