f50b31dd772598285849fd396a842c9651fa5d5c
nuuuwan <nuuuwan@gmail.com>
2025-08-03 08:19:30 +0530

    Added DocFactoryAggregated

--- src/lld/docs/DocFactory.py
+++ src/lld/docs/DocFactory.py
@@ -5,21 +5,12 @@ from utils import JSONFile, Log
 
 from lld.docs.abstract_doc import AbstractDoc
 from lld.docs.custom_docs import Act, Bill, ExtraGazette, Gazette
-from utils_future import Directory
+from lld.docs.DocFactoryAggregated import DocFactoryAggregated
 
 log = Log("DocFactory")
 
 
-class DocFactory:
-    DOCS_ALL_JSON_PATH = os.path.join(AbstractDoc.DIR_DATA, "all.json")
-    N_LATEST = 100
-
-    DOCS_LATEST_JSON_PATH = os.path.join(
-        AbstractDoc.DIR_DATA, f"latest-{N_LATEST}.json"
-    )
-    DOCS_TEMP_DATA_SUMMARY_JSON_PATH = os.path.join(
-        AbstractDoc.DIR_TEMP_DATA, "temp_data_summary.json"
-    )
+class DocFactory(DocFactoryAggregated):
 
     @staticmethod
     def cls_list_all():
@@ -72,52 +63,3 @@ class DocFactory:
         doc_list.sort(key=lambda x: (x.date, x.doc_num), reverse=True)
         log.debug(f"Found {len(doc_list):,} docs (all types).")
         return doc_list
-
-    @staticmethod
-    def x_to_list_all(doc_list, func_get_key):
-        doc_type_dict = {}
-        for doc in doc_list:
-            key = func_get_key(doc)
-            if key not in doc_type_dict:
-                doc_type_dict[key] = []
-            doc_type_dict[key].append(doc)
-        return doc_type_dict
-
-    @staticmethod
-    def get_total_data_size():
-        return Directory(AbstractDoc.DIR_DATA).size
-
-    @staticmethod
-    def write_all():
-        doc_list = DocFactory.list_all()
-        data_list = [doc.to_minimal_dict() for doc in doc_list]
-
-        for json_file_path, n in [
-            (DocFactory.DOCS_ALL_JSON_PATH, len(data_list)),
-            (
-                DocFactory.DOCS_LATEST_JSON_PATH,
-                min(DocFactory.N_LATEST, len(data_list)),
-            ),
-        ]:
-            JSONFile(json_file_path).write(data_list[:n])
-            file_size_k = os.path.getsize(json_file_path) / 1000
-            log.debug(f"Wrote {json_file_path} ({file_size_k:,.0f} KB)")
-
-    @staticmethod
-    def get_temp_data_summary(decade):
-        doc_list = DocFactory.list_all()
-        temp_data_summary = dict(
-            n_docs=len([d for d in doc_list if d.decade == decade]),
-            n_docs_with_pdfs=len([d for d in doc_list if d.n_pdfs > 0]),
-            n_pdfs=sum(d.n_pdfs for d in doc_list),
-            total_file_size=Directory(AbstractDoc.DIR_TEMP_DATA).size,
-        )
-        log.debug(f"{temp_data_summary=}")
-        return temp_data_summary
-
-    @staticmethod
-    def write_temp_data_summary(decade):
-        temp_data_summary = DocFactory.get_temp_data_summary(decade)
-        json_file_path = DocFactory.DOCS_TEMP_DATA_SUMMARY_JSON_PATH
-        JSONFile(json_file_path).write(temp_data_summary)
-        log.debug(f"Wrote {json_file_path} ({temp_data_summary})")
---
+++ src/lld/docs/DocFactoryAggregated.py
@@ -0,0 +1,54 @@
+import os
+
+from utils import JSONFile, Log
+
+from lld.docs.abstract_doc import AbstractDoc
+from utils_future import Directory
+
+log = Log("DocFactoryAggregated")
+
+
+class DocFactoryAggregated:
+    N_LATEST = 100
+    DOCS_ALL_JSON_PATH = os.path.join(AbstractDoc.DIR_TEMP_DATA, "all.json")
+    DOCS_LATEST_JSON_PATH = os.path.join(
+        AbstractDoc.DIR_TEMP_DATA, f"latest-{N_LATEST}.json"
+    )
+    DOCS_TEMP_DATA_SUMMARY_JSON_PATH = os.path.join(
+        AbstractDoc.DIR_TEMP_DATA, "temp_data_summary.json"
+    )
+
+    @classmethod
+    def write_all(cls):
+        doc_list = cls.list_all()
+        data_list = [doc.to_minimal_dict() for doc in doc_list]
+
+        for json_file_path, n in [
+            (cls.DOCS_ALL_JSON_PATH, len(data_list)),
+            (
+                cls.DOCS_LATEST_JSON_PATH,
+                min(cls.N_LATEST, len(data_list)),
+            ),
+        ]:
+            JSONFile(json_file_path).write(data_list[:n])
+            file_size_k = os.path.getsize(json_file_path) / 1000
+            log.debug(f"Wrote {json_file_path} ({file_size_k:,.0f} KB)")
+
+    @classmethod
+    def get_temp_data_summary(cls, decade):
+        doc_list = cls.list_all()
+        temp_data_summary = dict(
+            n_docs=len([d for d in doc_list if d.decade == decade]),
+            n_docs_with_pdfs=len([d for d in doc_list if d.n_pdfs > 0]),
+            n_pdfs=sum(d.n_pdfs for d in doc_list),
+            total_file_size=Directory(AbstractDoc.DIR_TEMP_DATA).size,
+        )
+        log.debug(f"{temp_data_summary=}")
+        return temp_data_summary
+
+    @classmethod
+    def write_temp_data_summary(cls, decade):
+        temp_data_summary = cls.get_temp_data_summary(decade)
+        json_file_path = cls.DOCS_TEMP_DATA_SUMMARY_JSON_PATH
+        JSONFile(json_file_path).write(temp_data_summary)
+        log.debug(f"Wrote {json_file_path} ({temp_data_summary})")
