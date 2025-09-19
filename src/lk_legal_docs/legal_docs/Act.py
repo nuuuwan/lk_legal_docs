2e8322d770473a4bead1695a701732aa5ba2cdf8
nuuuwan <nuuuwan@gmail.com>
2025-09-19 16:39:07 +0530

    remove print

--- src/lk_legal_docs/legal_docs/Act.py
+++ src/lk_legal_docs/legal_docs/Act.py
@@ -49,22 +49,8 @@ class Act(AbstractPDFDoc):
             lang_to_url_pdf[lang] = url_pdf
 
         for lang, url_pdf in lang_to_url_pdf.items():
-            act_number_cleaned = act_number.replace("/", "-").replace(
-                " ", "_"
-            )
+            act_number_cleaned = act_number.replace("/", "-").replace(" ", "_")
             num = f"{date_str}-{act_number_cleaned}-{lang}"
-            print(
-                dict(
-                    num=num,
-                    date_str=date_str,
-                    description=description,
-                    url_metadata=url_metadata,
-                    lang=lang,
-                    url_pdf=url_pdf,
-                    act_number=act_number,
-                )
-            )
-
             yield cls(
                 num=num,
                 date_str=date_str,
