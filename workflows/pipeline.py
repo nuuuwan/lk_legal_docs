01e57298608829ae0700dab40a665fc0044dccc8
nuuuwan <nuuuwan@gmail.com>
2025-09-19 18:04:52 +0530

    changed arg order

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -3,7 +3,7 @@ import sys
 from lk_legal_docs import Act, Bill
 
 if __name__ == "__main__":
-    doc_class_label = sys.argv[2]
+    doc_class_label = sys.argv[1]
     for doc_class in [Act, Bill]:
         if doc_class.get_doc_class_label() == doc_class_label:
             doc_class.run_pipeline()
