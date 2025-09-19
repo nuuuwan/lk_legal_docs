2e597a949e59960071cabbc29a7f2c0c3a96e2b0
nuuuwan <nuuuwan@gmail.com>
2025-09-19 15:05:32 +0530

    Cleanup files

--- src/utils_future/__init__.py
+++
@@ -1,8 +0,0 @@
-# utils_future (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from utils_future.Directory import Directory
-from utils_future.Git import Git
-from utils_future.Lang import Lang
-from utils_future.Markdown import Markdown
-from utils_future.PDF import PDF
--- tests/test_readme.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from lld import ReadMe
-
-
-class TestCase(unittest.TestCase):
-
-    def test_readme(self):
-        ReadMe().build()
