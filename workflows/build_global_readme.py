a39b20b83adbf12e3cd43f823a9332457317e2d5
nuuuwan <nuuuwan@gmail.com>
2025-10-08 09:38:36 +0530

    only two decades

--- workflows/build_global_readme.py
+++ workflows/build_global_readme.py
@@ -9,9 +9,6 @@ def main():
                 "lk_bills",
                 "lk_extraordinary_gazettes_2020s",
                 "lk_extraordinary_gazettes_2010s",
-                "lk_extraordinary_gazettes_2000s",
-                "lk_extraordinary_gazettes_1990s",
-                "lk_extraordinary_gazettes_1980s",
             ]
         }
     ).build()
